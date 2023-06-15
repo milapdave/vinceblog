172b5eca2284ebb4f6cda9e7302d1276384b99c1
milapdave <dave.milap3@gmail.com>
2023-06-15 18:46:58 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
  
 const Layout = ({ children, blok  }) => (
   <>
-    <div className={`${blok.content.layout === 'constrained' ? 'max-w-7xl mx-auto px-6' : ''} flex flex-col min-h-screen px-6 sm:px-0`}>
+    <div className={`${blok?.content?.layout === 'constrained' ? 'max-w-7xl mx-auto px-6' : ''} flex flex-col min-h-screen px-6 sm:px-0`}>
       <Navigation  blok={blok.content}/>
         {children}
     </div>
